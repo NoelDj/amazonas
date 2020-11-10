c16e950957801cf2b5ec579d78a37794961e01cb
NoelDj <noel0045@stud.kea.dk>
2020-11-10 18:51:19 +0100

    vamos

---
+++ script.js
@@ -0,0 +1,26 @@
+window.addEventListener("DOMContentLoaded", function(e){
+     fetch("http://aguacate.dk/pension_data/wp-json/wp/v2/categories")
+    .then(res => res.json())
+    .then(showCategory)
+})
+
+function showCategory(cat) {
+    /*document.querySelector(".category").textContent = cat.name;*/
+
+  cat.forEach(showPost)
+
+
+
+}
+
+function showPost(post){
+    console.log(post.name)
+
+    const template = document.querySelector("template").content;
+    const copy = template.cloneNode(true);
+
+    copy.querySelector("h3").textContent = post.name;
+
+    document.querySelector(".categories").appendChild(copy);
+
+}
